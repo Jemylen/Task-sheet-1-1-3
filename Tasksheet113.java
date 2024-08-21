163f244d706ae29e65a78f78915cd86b4af8f70c
Jemylen <mirandajemylen026@gmail.com>
2024-08-21 05:00:07 +0000

    Done

--- Tasksheet113.java
+++ Tasksheet113.java
@@ -1,1 +1,12 @@
+class Tasksheet113 {
+    public static void main(String[] args) {
+        int code = 3110;
+        byte zero = 0;
+        int number = 1;
+        double point = 2.0;
+        boolean truth = true;
+        String output =  "H" + code + " " + "w" + zero + "r" + number + "d" + " " + point + " " + truth;
+        System.out.println(output);
 
+    }
+}
